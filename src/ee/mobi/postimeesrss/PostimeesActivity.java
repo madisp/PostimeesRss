e77b4415ad7b59f987895f9d36496531cf0b0bcc
Madis Pink <madis.pink@gmail.com>
2011-10-24 12:38:19 +0300

    Fixed whitespace

--- src/ee/mobi/postimeesrss/PostimeesActivity.java
+++ src/ee/mobi/postimeesrss/PostimeesActivity.java
@@ -20,72 +20,72 @@ import android.view.Window;
 import android.widget.ListView;
 
 public class PostimeesActivity extends ListActivity {
-	
+
 	private RssLoadTask loader = new RssLoadTask();
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setProgressBarIndeterminate(true);
-		
-        setContentView(R.layout.main);
-		
-        loader.execute("http://www.postimees.ee/rss/");
-    }
-    
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-    	super.onListItemClick(l, v, position, id);
-    	ListItem item = (ListItem)l.getAdapter().getItem(position);
-    	startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(item.url)));
-    }
-    
-    private class RssLoadTask extends AsyncTask<String, Void, List<ListItem>> {
-    	@Override
-    	protected void onPreExecute() {
-    		super.onPreExecute();
-    		setProgressBarIndeterminateVisibility(true);
-    	}
-    	
-    	@Override
-    	protected List<ListItem> doInBackground(String... params) {
-    		List<ListItem> ret = new Vector<ListItem>();
-    		
-    		Resty resty = new Resty();
-    		for (String url : params) {
-    			try {
-    				XPath xpath = XPathFactory.newInstance().newXPath();
-    				
-    				NodeList items = resty.xml(url).get("/rss/channel/item");
-    				
+
+		setContentView(R.layout.main);
+
+		loader.execute("http://www.postimees.ee/rss/");
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		ListItem item = (ListItem) l.getAdapter().getItem(position);
+		startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(item.url)));
+	}
+
+	private class RssLoadTask extends AsyncTask<String, Void, List<ListItem>> {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			setProgressBarIndeterminateVisibility(true);
+		}
+
+		@Override
+		protected List<ListItem> doInBackground(String... params) {
+			List<ListItem> ret = new Vector<ListItem>();
+
+			Resty resty = new Resty();
+			for (String url : params) {
+				try {
+					XPath xpath = XPathFactory.newInstance().newXPath();
+
+					NodeList items = resty.xml(url).get("/rss/channel/item");
+
 					for (int i = 0; i < items.getLength(); i++) {
-						
+
 						ListItem listItem = new ListItem();
 						Node item = items.item(i);
-						
+
 						listItem.title = xpath.evaluate("title/text()", item);
 						listItem.url = xpath.evaluate("link/text()", item);
 						listItem.pubDate = xpath.evaluate("pubDate/text()", item);
-						
+
 						ret.add(listItem);
-						
+
 					}
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-    		}
-    		
-    		return ret;
-    	}
-    	
-    	@Override
-    	protected void onPostExecute(List<ListItem> result) {
-    		super.onPostExecute(result);
-    		setProgressBarIndeterminateVisibility(false);
-    		setListAdapter(new RssAdapter(PostimeesActivity.this, result));
-    	}
-    }
+			}
+
+			return ret;
+		}
+
+		@Override
+		protected void onPostExecute(List<ListItem> result) {
+			super.onPostExecute(result);
+			setProgressBarIndeterminateVisibility(false);
+			setListAdapter(new RssAdapter(PostimeesActivity.this, result));
+		}
+	}
 }
--- src/ee/mobi/postimeesrss/RssAdapter.java
+++ src/ee/mobi/postimeesrss/RssAdapter.java
@@ -9,10 +9,10 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class RssAdapter extends BaseAdapter {
-	
+
 	private Context context;
 	private List<ListItem> items;
-	
+
 	public RssAdapter(Context context, List<ListItem> items) {
 		this.context = context;
 		this.items = items;
@@ -36,16 +36,19 @@ public class RssAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		if (convertView == null) {
-			convertView = View.inflate(context, android.R.layout.simple_list_item_2, null);
+			convertView = View.inflate(context,
+					android.R.layout.simple_list_item_2, null);
 		}
-		TextView primary = (TextView)convertView.findViewById(android.R.id.text1);
-		TextView secondary = (TextView)convertView.findViewById(android.R.id.text2);
-		
-		ListItem listItem = (ListItem)getItem(position);
-		
+		TextView primary = (TextView) convertView
+				.findViewById(android.R.id.text1);
+		TextView secondary = (TextView) convertView
+				.findViewById(android.R.id.text2);
+
+		ListItem listItem = (ListItem) getItem(position);
+
 		primary.setText(listItem.title);
 		secondary.setText(listItem.pubDate);
-		
+
 		return convertView;
 	}
 }
